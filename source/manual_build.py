76c5de78842ae10ddabd4d4b1bf0d050aa3bd65a
Sean Smith <seaam@amazon.com>
2020-08-03 15:21:59 -0700

    Upload Template to S3
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- source/manual_build.py
+++ source/manual_build.py
@@ -3,7 +3,32 @@ import os
 import random
 import string
 import sys
+import boto3
+from colored import fg, bg, attr
+from botocore.client import ClientError
 from shutil import make_archive, copy, copytree
+
+def upload_objects(s3, bucket_name, s3_prefix, directory_name):
+    try:
+        my_bucket = s3.Bucket(bucket_name)
+        for path, subdirs, files in os.walk(directory_name):
+            path = path.replace("\\","/")
+            directory = path.replace(directory_name,"")
+            for file in files:
+                print("%s[+] Uploading %s to s3://%s/%s%s%s" % (fg('green'), os.path.join(path, file), bucket_name, s3_prefix, directory+'/'+file, attr('reset')))
+                my_bucket.upload_file(os.path.join(path, file), s3_prefix+directory+'/'+file)
+
+    except Exception as err:
+        print(err)
+
+
+def get_input(prompt):
+    if sys.version_info[0] >= 3:
+        response = input(prompt)
+    else:
+        #Python 2
+        response = raw_input(prompt)
+    return response
 
 if __name__ == "__main__":
     build_path = os.path.dirname(os.path.realpath(__file__))
@@ -31,21 +56,30 @@ if __name__ == "__main__":
     print(" > Creating archive for build id: " + unique_id)
     make_archive('dist/' + output_prefix, 'gztar', build_folder)
 
-    print("\n====== Build COMPLETE ======")
+    print("====== Upload to S3 ======\n")
+    s3 = boto3.resource('s3')
+    region = get_input(" > Please enter the AWS region you'd like to build SOCA in: ")
+    bucket_name = get_input(" > Please enter the name of an S3 bucket you own: ")
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+    except ClientError:
+        bucket_name = input(" > The bucket "+ bucket_name + " does not exist or you have no access.")
+    print(" > Uploading required files ... ")
+    upload_objects(s3, bucket_name, output_prefix, build_path + "/" + build_folder)
+
+    # CloudFormation Template URL
+    template_url = "https://%s.s3.amazonaws.com/%s/scale-out-computing-on-aws.template" % (bucket_name, output_prefix)
+
+    print("\n====== Upload COMPLETE ======")
     print("\n====== Installation Instructions ======")
-    print("1: Create or use an existing S3 bucket on your AWS account (eg: 'mysocacluster')")
-    print("2: Drag & Drop " + build_path + "/" + build_folder + " to your S3 bucket (eg: 'mysocacluster/" + build_folder + ")")
-    print("3: Launch CloudFormation and use scale-out-computing-on-aws.template as base template")
-    print("4: Enter your cluster information.")
+    print("1. Click on the following link:")
+    print("%shttps://console.aws.amazon.com/cloudformation/home?region=%s#/stacks/create/review?&templateURL=%s&param_S3InstallBucket=%s&param_S3InstallFolder=%s%s" % (fg('blue'), region, template_url, bucket_name, output_prefix, attr('reset')))
+    print("2. The 'Install Location' parameters are pre-filled for you, fill out the rest of the parameters.")
     print("")
     print("")
     print("For more information: https://awslabs.github.io/scale-out-computing-on-aws/install-soca-cluster/")
 
-    if sys.version_info[0] >= 3:
-        input("Press Enter key to close ..")
-    else:
-        #Python 2
-        raw_input("Press Enter key to close ..")
+    get_input("Press Enter key to close ..")
 
 
 
