812f9aa04c81036daa49cf15655c3a8b9a63391a
mcrozes <mcrozes@amazon.com>
2021-03-22 17:16:47 -0400

    Fixed new broken links due to mkdocs system update

--- source/manual_build.py
+++ source/manual_build.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         print("%sSorry, Windows builds are currently not supported. Please use a UNIX system if you want to do a custom build\n%s" % (fg('yellow'), attr('reset')))
         print("%s=== How to install SOCA on Windows ===%s" % (fg('yellow'), attr('reset')))
         print("%s1 - Download the latest release (RELEASE-<version>.tar.gz) from https://github.com/awslabs/scale-out-computing-on-aws/releases%s" % (fg('yellow'), attr('reset')))
-        print("%s2 - Install SOCA via https://awslabs.github.io/scale-out-computing-on-aws/install-soca-cluster/#option-2-download-the-latest-release-targz%s"  % (fg('yellow'), attr('reset')))
+        print("%s2 - Install SOCA via https://awslabs.github.io/scale-out-computing-on-aws/tutorials/install-soca-cluster/#option-2-download-the-latest-release-targz%s"  % (fg('yellow'), attr('reset')))
         exit(1)
 
     parser = argparse.ArgumentParser(description='Build & Upload SOCA CloudFormation resources.')
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         print("3: Launch CloudFormation and use scale-out-computing-on-aws.template as base template")
         print("4: Enter your cluster information.")
 
-    print("\n\nFor more information: https://awslabs.github.io/scale-out-computing-on-aws/install-soca-cluster/")
+    print("\n\nFor more information: https://awslabs.github.io/scale-out-computing-on-aws/tutorials/install-soca-cluster/")
 
 
 
