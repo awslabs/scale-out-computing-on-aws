d030acfe9818223c95cbbf7460c2cf99a98439c9
mcrozes <mcrozes@amazon.com>
2021-12-09 17:31:49 -0500

    Fixed SolVersion

--- source/soca/cluster_web_ui/config.py
+++ source/soca/cluster_web_ui/config.py
@@ -22,7 +22,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def boto_extra_config():
-    aws_solution_user_agent = {"user_agent_extra": "AwsSolution/SO0072/__VERSION__"}
+    aws_solution_user_agent = {"user_agent_extra": "AwsSolution/SO0072/v2.7.0"}
     return botocore_config.Config(**aws_solution_user_agent)
 
 class Config(object):
