00ef278138ed1fa6db73e6331abcfcd9d22177cf
NingLyu <lvning19860909@gmail.com>
2025-06-23 03:29:48 +0000

    feat: update cdk

--- installer/resources/src/cdk_construct.py
+++ installer/resources/src/cdk_construct.py
@@ -6672,7 +6672,6 @@ class SOCAInstall(Stack):
 
 if __name__ == "__main__":
     app = App()
-    print("lvning test in main cdk")
     print(app.node.try_get_context("region"))
 
     # User specified variables/install properties, queryable as Python Object
@@ -6794,13 +6793,13 @@ if __name__ == "__main__":
     }
 
     principals_suffix = {
-        "backup": f"backup.{Aws.URL_SUFFIX}",
-        "cloudwatch": f"cloudwatch.{Aws.URL_SUFFIX}",
-        "ec2": f"ec2.{Aws.URL_SUFFIX}",
-        "lambda": f"lambda.{Aws.URL_SUFFIX}",
-        "sns": f"sns.{Aws.URL_SUFFIX}",
-        "spotfleet": f"spotfleet.{Aws.URL_SUFFIX}",
-        "ssm": f"ssm.{Aws.URL_SUFFIX}",
+        "backup": "backup.amazonaws.com",
+        "cloudwatch": "cloudwatch.amazonaws.com",
+        "ec2": "ec2.amazonaws.com",
+        "lambda": "lambda.amazonaws.com",
+        "sns": "sns.amazonaws.com",
+        "spotfleet": "spotfleet.amazonaws.com",
+        "ssm": "ssm.amazonaws.com",
     }
 
     # Apply default tag to all taggable resources
